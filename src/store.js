68de54a85b914a3089e7a70d1e36399f2a2cc36d
shubhada yadav <shubhada.yadav107@gmail.com>
2023-07-26 13:40:29 +0530

    changes complete

--- src/store.js
+++ src/store.js
@@ -1,32 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterslice from "../../Counter/counterslice";
+// import counterslice from "../../Counter/counterslice";
 import cartReducer from "../cartSlice/cartSlice";
 
-
-
-
-
-
-
-
 const Store = configureStore({
+  reducer: {
+    // counterChange: counterslice,
+    cart: cartReducer,
+  },
 
-        reducer: {
-            counterChange: counterslice,
-            cart: cartReducer,
-        },
-
-        legacy_createStore: {
-            legacy_createStore: (
-                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-            )
-        }
-
-
-    },
-
-
-
-)
+  legacy_createStore: {
+    legacy_createStore:
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  },
+});
 
 export default Store;
